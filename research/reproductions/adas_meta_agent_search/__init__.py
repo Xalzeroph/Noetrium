4ee5db29878a4ce840c53042061210cb0eeee5bd
Xu Zihan <2024010904024@std.uestc.edu.cn>
2026-09-16 00:42:16 +0800

    feat(reproduction): expose ADAS fidelity

---
+++ research/reproductions/adas_meta_agent_search/__init__.py
@@ -0,0 +1,11 @@
+from .fidelity import (
+    ADAS_AUDITED_COMMIT,
+    ADAS_META_AGENT_SEARCH_FIDELITY,
+    ADASMetaAgentSearchFidelity,
+)
+
+__all__ = [
+    "ADAS_AUDITED_COMMIT",
+    "ADAS_META_AGENT_SEARCH_FIDELITY",
+    "ADASMetaAgentSearchFidelity",
+]
