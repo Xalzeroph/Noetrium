c27b2dc3159d3b17562fd280bb7ecc39394a7651
Xu Zihan <2024010904024@std.uestc.edu.cn>
2026-09-15 21:37:37 +0800

    refactor(agent): defer turn fact public ABI promotion

--- noetrium_platform/capabilities/participant/agent/api/__init__.py
+++ noetrium_platform/capabilities/participant/agent/api/__init__.py
@@ -41,11 +41,6 @@ from .skill_checkpoint import (
     AGENT_SKILL_LIBRARY_CHECKPOINT_SCHEMA,
     AgentSkillLibraryCheckpoint,
 )
-from .turn_facts import (
-    AGENT_TURN_FACT_SCHEMA,
-    AgentTurnFact,
-    AgentTurnFactKind,
-)
 from .cognition_ports import (
     AgentActionExecutorPort,
     AgentCompletionPort,
@@ -65,7 +60,6 @@ __all__ = [
     "AGENT_COORDINATION_CHECKPOINT_SCHEMA",
     "AGENT_MEMORY_CHECKPOINT_SCHEMA",
     "AGENT_SKILL_LIBRARY_CHECKPOINT_SCHEMA",
-    "AGENT_TURN_FACT_SCHEMA",
     "AgentConversationCheckpoint",
     "AgentConversationMessageCheckpoint",
     "AgentConversationSessionCheckpoint",
@@ -112,8 +106,6 @@ __all__ = [
     "AgentSkillRecord",
     "AgentSkillSelection",
     "AgentStepReceipt",
-    "AgentTurnFact",
-    "AgentTurnFactKind",
     "action_summary_payload",
     "JsonObject",
     "JsonValue",
