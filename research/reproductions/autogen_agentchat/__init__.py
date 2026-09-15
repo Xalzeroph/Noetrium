d34cbbb8892a93ecda67dc3f8ea1d4dc6b6ee7d4
Xu Zihan <2024010904024@std.uestc.edu.cn>
2026-09-16 00:52:48 +0800

    feat(reproduction): expose AutoGen AgentChat fidelity

---
+++ research/reproductions/autogen_agentchat/__init__.py
@@ -0,0 +1,11 @@
+from .fidelity import (
+    AUTOGEN_AGENTCHAT_FIDELITY,
+    AUTOGEN_PAPER_ERA_COMMIT,
+    AutoGenAgentChatFidelity,
+)
+
+__all__ = [
+    "AUTOGEN_AGENTCHAT_FIDELITY",
+    "AUTOGEN_PAPER_ERA_COMMIT",
+    "AutoGenAgentChatFidelity",
+]
