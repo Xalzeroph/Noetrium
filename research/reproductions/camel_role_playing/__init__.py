8fd6de356fb7427ad09ce52e2e49232ad3491043
Xu Zihan <2024010904024@std.uestc.edu.cn>
2026-09-16 00:49:27 +0800

    feat(reproduction): expose CAMEL role-playing fidelity

---
+++ research/reproductions/camel_role_playing/__init__.py
@@ -0,0 +1,11 @@
+from .fidelity import (
+    CAMEL_PAPER_ERA_COMMIT,
+    CAMEL_ROLE_PLAYING_FIDELITY,
+    CAMELRolePlayingFidelity,
+)
+
+__all__ = [
+    "CAMEL_PAPER_ERA_COMMIT",
+    "CAMEL_ROLE_PLAYING_FIDELITY",
+    "CAMELRolePlayingFidelity",
+]
