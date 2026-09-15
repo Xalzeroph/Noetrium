1ebad2ae616f38ea7856f0165442e8a85211cb73
Xu Zihan <2024010904024@std.uestc.edu.cn>
2026-09-15 23:40:46 +0800

    feat(repro): expose Reflexion ALFWorld package

---
+++ research/reproductions/reflexion_alfworld/__init__.py
@@ -0,0 +1,9 @@
+from .fidelity import REFLEXION_ALFWORLD_FIDELITY, ReflexionAlfworldFidelity
+from .semantics import ReflexionTaskState, should_reflect
+
+__all__ = [
+    "REFLEXION_ALFWORLD_FIDELITY",
+    "ReflexionAlfworldFidelity",
+    "ReflexionTaskState",
+    "should_reflect",
+]
