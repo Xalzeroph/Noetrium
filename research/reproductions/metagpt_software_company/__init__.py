271a633755381c5a411e6e775e38e9c9f10ad493
Xu Zihan <2024010904024@std.uestc.edu.cn>
2026-09-16 00:51:51 +0800

    feat(reproduction): expose MetaGPT software-company fidelity

---
+++ research/reproductions/metagpt_software_company/__init__.py
@@ -0,0 +1,11 @@
+from .fidelity import (
+    METAGPT_PAPER_ERA_COMMIT,
+    METAGPT_SOFTWARE_COMPANY_FIDELITY,
+    MetaGPTSoftwareCompanyFidelity,
+)
+
+__all__ = [
+    "METAGPT_PAPER_ERA_COMMIT",
+    "METAGPT_SOFTWARE_COMPANY_FIDELITY",
+    "MetaGPTSoftwareCompanyFidelity",
+]
