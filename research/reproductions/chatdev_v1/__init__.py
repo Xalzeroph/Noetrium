364efbc3ce03604ddc17c7dfc9e42587d3933970
Xu Zihan <2024010904024@std.uestc.edu.cn>
2026-09-16 10:57:06 +0800

    feat(reproduction): expose ChatDev v1 scaffold

---
+++ research/reproductions/chatdev_v1/__init__.py
@@ -0,0 +1,31 @@
+from .chain import (
+    CHATDEV_V1_COMPOSED_PHASES,
+    CHATDEV_V1_SIMPLE_PHASES,
+    CHATDEV_V1_TOP_LEVEL_CHAIN,
+    ChatDevV1ComposedPhaseSpec,
+    ChatDevV1PhaseKind,
+    ChatDevV1PhaseReceipt,
+    ChatDevV1SimplePhaseSpec,
+)
+from .fidelity import (
+    CHATDEV_V1_AUDITED_COMMIT,
+    CHATDEV_V1_AUDITED_TAG,
+    CHATDEV_V1_RECRUITMENTS,
+    CHATDEV_V1_REFERENCE_FIDELITY,
+    ChatDevV1ReferenceFidelity,
+)
+
+__all__ = [
+    "CHATDEV_V1_AUDITED_COMMIT",
+    "CHATDEV_V1_AUDITED_TAG",
+    "CHATDEV_V1_COMPOSED_PHASES",
+    "CHATDEV_V1_RECRUITMENTS",
+    "CHATDEV_V1_REFERENCE_FIDELITY",
+    "CHATDEV_V1_SIMPLE_PHASES",
+    "CHATDEV_V1_TOP_LEVEL_CHAIN",
+    "ChatDevV1ComposedPhaseSpec",
+    "ChatDevV1PhaseKind",
+    "ChatDevV1PhaseReceipt",
+    "ChatDevV1ReferenceFidelity",
+    "ChatDevV1SimplePhaseSpec",
+]
